047c605f69b71fea825a8085cc00e40e3c738f4a
Eliot Blennerhassett <eliot@eliot.lan>
2014-09-18 11:57:47 +1200

    Add a contestant class, gets rid of lots of array indexing

--- 5/let-the-games-begin.py
+++ 5/let-the-games-begin.py
@@ -7,56 +7,66 @@ Eliot Blennerhassett
 print("Kiwi PyCon 2014 codewars problem 5 - RPS contest")
 print("Extract contestant data by doing 'tar xf 5-rock-paper-scissors'")
 
-cl = []  # contestant list
-for ci in range(1,65):
+class contestant(object):
+    def __init__(self, index, data):
+        data = [l.strip() for l in data]
+        self.name = data[0].split('=')[1]
+        self.move_idx = 0
+        self.moves = data[1:]
+        self.index = index
+
+    def next_move(self):
+        nm = self.moves[self.move_idx]
+        self.move_idx += 1
+        return nm
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+contestants = []
+for ci in range(1,99):
     fn = 'contestants/contestant%02d' % ci
-    with open(fn) as f:
-        ll = [l.strip() for l in f.readlines()]
-        ll[0] = ll[0].split('=')[1] # just name
-        cl.append(ll)
+    try:
+        with open(fn) as f:
+            contestants.append(contestant(ci, f.readlines()))
+    except FileNotFoundError:
+        break
 
 
-# Assuming that the moves get used up, keep track of which move to use next
-next_move = [1] * 64  # index 0 is contestant name
 
+def play(c1, c2):
+    m1 = c1.next_move()
+    m2 = c2.next_move()
+    print('1,2,3... %s x %s' % (m1, m2))
 
-def battle(c1, c2):
-    ml1 = cl[c1]
-    ml2 = cl[c2]
-    winner = None
-    mi1 = next_move[c1]
-    mi2 = next_move[c2]
-    n1 = ml1[0]
-    n2 = ml2[0]
-    print('%s against %s' % (n1, n2))
+    if m1 == m2:
+        winner = None
+    elif ((m1=='rock') and (m2=='scissors') or
+          (m1=='scissors') and (m2=='paper') or
+          (m1=='paper') and (m2=='rock')):
+        winner = c1
+    else:
+        winner = c2
+        print('%s wins' % winner)
 
-    for mi in range(1000):
-        m1 = ml1[mi1]
-        m2 = ml2[mi2]
-        mi1 += 1
-        mi2 += 1
-        print('Play <%s> <%s>' % (m1, m2))
-        if m1 == m2:
-            continue
-        elif ((m1=='rock') and (m2=='scissors') or
-              (m1=='scissors') and (m2=='paper') or
-              (m1=='paper') and (m2=='rock')):
-            winner = c1
-            break
-        else:
-            winner = c2
-            break
-
-    next_move[c1] = mi1
-    next_move[c2] = mi2
-
-    print('%s wins' % cl[winner][0])
     return winner
 
-contestants = range(0,64)
+def battle(c1, c2):
+    print('%s against %s' % (c1, c2))
 
-for round in range(1, 100):
-    print('************* Round %d ************' % round)
+    for mi in range(1000):
+        winner = play(c1, c2)
+        if winner is not None:
+            break
+
+    return winner
+
+
+for r in range(1, 100):
+    print('************* Round %d ************' % r)
     winners = []
     for ci in range(0, len(contestants), 2):
         winners.append(battle(contestants[ci], contestants[ci + 1]))
@@ -64,20 +74,15 @@ for round in range(1, 100):
     if len(contestants) == 2:
         break
     contestants = winners
-
+    # print(contestants)
 
 winner = winners[0]
-winner_name = cl[winner][0]
-print('Winner is', winner_name)
 for c in contestants:
-    if c != winner:
+    if c.name != winner.name:
         second = c
-        second_name = cl[second][0]
-        print('Second is', second_name)
 
-secret = winner_name + second_name
-print('Secret is', secret)
-
-
-
-
+print('-------------------- The contest is over! -------------------')
+print('Second is', second.name)
+print('Winner is', winner.name)
+secret = winner.name + second.name
+print('********************\nSecret is', secret)
