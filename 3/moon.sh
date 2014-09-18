56fff6a0ecdb065a5eb4264c35187a8311ef13ef
Eliot Blennerhassett <eliot@eliot.lan>
2014-09-18 16:24:12 +1200

    Make my extract-page-text.py redundant.
    Use pdftotext instead.
    Now solution is all trad unix commandline tools

--- 3/moon.sh
+++ 3/moon.sh
@@ -6,15 +6,24 @@ file mmmmm-tasty-tasty-data
 mv mmmmm-tasty-tasty-data data.xxd.gz
 gunzip -f data.xxd.gz
 
-head data.xxd
-./unhexdump.py data.xxd data.zip
+head -n4 data.xxd
+# Make my unhexdump.py redundant, thanks Team Cats
+# whoda thunk this would exist?
+xxd -r data.xxd > data.zip
 
 file data.zip
-unzip -o data.zip
+unzip -q -o data.zip
 # get pages/*.pdf
 
 cd pages
-../extract-page-text.py ../pages.txt
+rm -f pages.txt
+for f in one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twenty-one twenty-two twenty-three twenty-four twenty-five twenty-six twenty-seven
+do
+pdftotext -nopgbrk "page-$f.pdf"
+cat "page-$f.txt" >> pages.txt
+rm "page-$f.txt"
+done
+mv pages.txt ..
 cd ..
 
 base64 -d pages.txt > pages.png
